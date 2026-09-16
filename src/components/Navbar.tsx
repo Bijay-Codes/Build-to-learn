249fec6a0b79706e1375f6f470ff5fc5340bec18
Bijay-Codes <bijayprojects@gmail.com>
2026-09-16 14:16:41 +0530

    reworked the navbar to show active link and updated how the projects show up

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,19 +2,21 @@ import { NavLink } from 'react-router';
 import { pagePaths } from '../pages/Pages';
 
 export function Navbar() {
+    const selectedLink = 'border-b-4 border-primary-bg font-extrabold';
+
     return (
-        <nav className='bg-surface-bg text-surface-fg w-full p-4 min-h-15 sticky top-0 z-50 border-b border-b-accent-bg'>
+        <nav className='bg-surface-bg text-surface-fg w-full p-4 min-h-15 sticky top-0 z-50 border-b border-b-accent-bg/50'>
             <ol className='flex gap-4 text-xl'>
                 <li>
                     <NavLink
                         to={pagePaths.home}
-                        className={(selected) => selected ? 'selected' : 'normal'}>
+                        className={({ isActive }) => isActive ? selectedLink : ''}>
                         Home
                     </NavLink>
                 </li>
                 <li>
                     <NavLink to={pagePaths.intro}
-                        className={(selected) => selected ? 'selected' : 'normal'}>
+                        className={({ isActive }) => isActive ? selectedLink : ''}>
                         Introduction
                     </NavLink>
                 </li>
--- src/components/RenderProblems.tsx
+++ src/components/RenderProblems.tsx
@@ -2,31 +2,39 @@ import { useNavigate } from "react-router";
 import { pagePaths } from "../pages/Pages";
 export function Problems() {
     const navigate = useNavigate();
+    const problemStyles = 'bg-surface-bg text-surface-fg p-4 outline outline-accent-bg/20 max-w-100 flex flex-col gap-4 justify-center items-center rounded hover:bg-surface-muted-bg hover:text-surface-muted-fg'
     return (
         <section className="p-10">
-            <main className="flex flex-col flex-wrap gap-6">
-                <div onClick={() => navigate(pagePaths.morseCodeTranslator)}>
+            <main className="flex flex-wrap gap-6">
+                <div onClick={() => navigate(pagePaths.morseCodeTranslator)} className={problemStyles}>
                     <h1 className="text-2xl">Text to morse code translator</h1>
-                    <h2>
+                    <h2 className="text-lg">
                         A translator tool that converts text to morsecode with copy feature...
                     </h2>
                     <p className="max-w-120">
                         I have literally nothing to say on this excersise tbh i didnt learnt to do something while making this one.
                         idk why i'm even listing it here
                     </p>
+                    <span className="text-center mt-auto text-accent-bg">Click to see it working</span>
                 </div>
-                <div onClick={() => navigate(pagePaths.svgpractice)}>
-                    <h1>SVG practicing</h1>
-                    <h2>Practicing to animate SVGs, manupulate them with diffrent methods/approach that come with them to make the sites feel alive</h2>
+                <div onClick={() => navigate(pagePaths.svgpractice)} className={problemStyles}>
+                    <h1 className="text-2xl">SVG practicing</h1>
+                    <h2 className="text-lg">Practicing to animate SVGs, manupulate them with diffrent methods/approach that come with them to make the sites feel alive</h2>
+                    <span className="text-center mt-auto text-accent-bg">Click to see it working</span>
                 </div>
-                <div
+                <div className={problemStyles}
                     onClick={() => navigate(pagePaths.reactActionState)}>
-                    Practicing react hooks like useActionState to elevate skills
+                    <h1 className="text-2xl">React hooks</h1>
+                    <p className="text-lg">Practicing react hooks like useActionState to elevate skills with,
+                        Key observations: it is just doing extra work for us instead us tracking and calling the error we leave it to the state we just have to handle what happens when there is an error</p>
+                    <span className="text-center mt-auto text-accent-bg">Click to see it working</span>
                 </div>
-                <div onClick={() => navigate(pagePaths.mouseTracker)}>
-                    Practicing tracking mouse cordinates to style the page in a more pleasing way
+                <div className={problemStyles} onClick={() => navigate(pagePaths.mouseTracker)}>
+                    <h1 className="text-2xl">Cursor trail animation</h1>
+                    <p className="text-lg">Practicing tracking mouse cordinates to style the page in a more pleasing way, Just a fun little excersise to learn the window syntax of how to track and animate the position of something that follows the cursor constantly</p>
+                    <span className="text-center mt-auto text-accent-bg">Click to see it working</span>
                 </div>
             </main>
-        </section>
+        </section >
     )
 }
